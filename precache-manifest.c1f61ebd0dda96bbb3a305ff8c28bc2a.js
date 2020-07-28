9b382ca1a60cce95de0c8d393c877aa50a6ccf78
unknown <mukherjeerivu7@gmail.com>
2020-07-28 21:37:36 +0530

    Updates

---
+++ precache-manifest.c1f61ebd0dda96bbb3a305ff8c28bc2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a39cfe8ba5d338842f79b4eba7064f7",
+    "url": "/react-spinner-demo/index.html"
+  },
+  {
+    "revision": "2e76d18a2a390f8070be",
+    "url": "/react-spinner-demo/static/css/main.71ca9613.chunk.css"
+  },
+  {
+    "revision": "ba1dfaaf3b9006ff60fb",
+    "url": "/react-spinner-demo/static/js/2.3b379181.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-spinner-demo/static/js/2.3b379181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e76d18a2a390f8070be",
+    "url": "/react-spinner-demo/static/js/main.8eaa3f5c.chunk.js"
+  },
+  {
+    "revision": "cad301dedebf489f3423",
+    "url": "/react-spinner-demo/static/js/runtime-main.7a9d70b2.js"
+  }
+]);
