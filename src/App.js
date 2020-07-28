651bbeecf281be7c24b31eb309bf53e5cda7b627
unknown <mukherjeerivu7@gmail.com>
2020-07-28 21:32:24 +0530

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { css } from '@emotion/core'
+import { BounceLoader, BarLoader, BeatLoader } from 'react-spinners'
 import './App.css';
+
+const loaderCSS = css`
+margin-top: 25px;
+margin-bottom: 25px;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <BounceLoader css={loaderCSS} size={48} color='orange' loading/>
+
+     <BarLoader css={loaderCSS} size={48} color='red' loading/>
+
+     <BeatLoader css={loaderCSS} size={48} color='purple' loading/>
     </div>
   );
 }
